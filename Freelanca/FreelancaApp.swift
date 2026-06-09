b5e548ad94c2fd41d20c03c44c50af32965b5f1c
mangochops <whymangoeswillicent@gmail.com>
2026-06-09 13:18:34 +0300

    Initial Commit

---
+++ Freelanca/FreelancaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreelancaApp.swift
+//  Freelanca
+//
+//  Created by mac on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct FreelancaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
